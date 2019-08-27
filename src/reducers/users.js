f4dfe5d43604d0ee229b12bb8c0e361ce1acbca1
Michael YOON <myoon@plan-group.com>
2019-08-27 08:24:18 -0400

    feat:add thunk

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,6 +1,6 @@
 import { RECEIVE_USERS } from '../actions/users'
 
-export default function tweets(state = {}}, action) {
+export default function tweets(state = {}, action) {
     switch (action.type) {
         case RECEIVE_USERS:
             ...state,
