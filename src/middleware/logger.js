3c7b4fd3e5659977948cb2b5686c77406366f1fa
Michael YOON <myoon@plan-group.com>
2019-09-03 08:33:27 -0400

    fix trivial

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,8 +1,8 @@
 const logger = (store) => (next) => (action) => {
     console.group(action.type)
-        console.log('The action: ', action)
-        const returnVlaue = next(action)
-        console.log('The new state: ', store.getState())
+    console.log('The action: ', action)
+    const returnValue = next(action)
+    console.log('The new state: ', store.getState())
     console.groupEnd()
     return returnValue
 }
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -3,8 +3,10 @@ import { RECEIVE_USERS } from '../actions/users'
 export default function tweets(state = {}, action) {
     switch (action.type) {
         case RECEIVE_USERS:
-            ...state,
-            ...action.users
+            return {
+                ...state,
+                ...action.users
+            }
         default:
             return state
     }
